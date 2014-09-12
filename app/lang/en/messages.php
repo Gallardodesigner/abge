f0e1896f78f26f8d0160f71edfad858698278fdd
AlexanderZon <alexisanderson@ovi.com>
2014-09-12 14:15:27 -0430

    Languages Adapters

---
+++ app/lang/en/messages.php
@@ -0,0 +1,80 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Pagination Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are used by the paginator library to build
+	| the simple pagination links. You are free to change them to anything
+	| you want to customize your views to better match your application.
+	|
+	*/
+
+	# Category Messages
+
+	'categories_create' => 'The category :title was created successfully',
+
+	'categories_create_err' => 'Oops, we had a problem creating the category :title',
+
+	'categories_update' => 'The category :title was updated succesfully',
+
+	'categories_update_err' => 'Oops, we had a problem updating the category',
+
+	'categories_trash' => 'The category :title was sent to the recicle bin',
+
+	'categories_trash_err' => 'Oops, we had a problem sendig the category to the recicle bin',
+
+	'categories_publish' => 'The category :title was published succesfully',
+
+	'categories_publish_err' => 'Oops, we had a problem publishing the category :title',
+
+	'categories_draft' => 'The category :title has been changed to draft mode',
+
+	'categories_draft_err' => 'Oops, we had a problem changing the category :title to draft mode',
+
+	'categories_untrash' => 'The category :title was successfully restored',
+
+	'categories_untrash_err' => 'Oops, we had a problem restoring the category :title',
+
+	'categories_delete' => 'The category :title was deleted succesfully',
+
+	'categories_delete_err' => 'Oops, we had a problem deleting the category :title',
+
+	'categories_display_err' => 'Oops, we could not find the category',
+
+	# Company Messages
+
+	'companies_create' => 'The company :title was created successfully',
+
+	'companies_create_err' => 'Oops, we had a problem creating the company :title',
+
+	'companies_update' => 'The company :title was updated succesfully',
+
+	'companies_update_err' => 'Oops, we had a problem updating the company',
+
+	'companies_trash' => 'The company :title was sent to the recicle bin',
+
+	'companies_trash_err' => 'Oops, we had a problem sendig the company to the recicle bin',
+
+	'companies_publish' => 'The company :title was published succesfully',
+
+	'companies_publish_err' => 'Oops, we had a problem publishing the company :title',
+
+	'companies_draft' => 'The company :title has been changed to draft mode',
+
+	'companies_draft_err' => 'Oops, we had a problem changing the company :title to draft mode',
+
+	'companies_untrash' => 'The company :title was successfully restored',
+
+	'companies_untrash_err' => 'Oops, we had a problem restoring the company :title',
+
+	'companies_delete' => 'The company :title was deleted succesfully',
+
+	'companies_delete_err' => 'Oops, we had a problem deleting the company :title',
+
+	'companies_display_err' => 'Oops, we could not find the company',
+
+);
