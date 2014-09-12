c5e1e34c9a8269c9607feb0e9fe617dd290368dc
AlexanderZon <alexisanderson@ovi.com>
2014-09-12 02:46:31 -0430

    CompanyController Structure completed

--- app/controllers/CompanyController.php
+++ app/controllers/CompanyController.php
@@ -2,85 +2,216 @@
 
 class CompanyController extends \BaseController {
 
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
-		//
+	
+	protected $route = '/dashboard/companies';
+
+	public function getIndex(){
+
+		$companies = Companies::getUntrash();
+
+		$msg_success = Session::get('msg_success');
+
+		$msg_error = Session::get('msg_error');
+
+		return View::make('backend.companies.index', array(
+			'companies' => $companies,
+			'msg_success' => $msg_success,
+			'msg_error' => $msg_error
+			));
+
 	}
 
+	public function getCreate(){
 
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-		//
+		return View::make('backend.companies.create');
+
 	}
 
+	public function postCreate(){
 
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @return Response
-	 */
-	public function store()
-	{
-		//
+		$company = new Companies();
+		$company->title = Input::get('title');
+		$company->content = Input::get('content');
+		$company->type = 'company';
+		$company->status = 'draft';
+		$company->save();
+
+		return View::make('backend.companies.index', array('msg_success' => 'The company was successfully created!'));
+
 	}
 
+	public function getUpdate( $id = '' ){
 
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
-		//
+		if( $id == '' ):
+
+			return Redirect::to($this->route);
+		
+		else:
+
+			$company = Companies::find($id);
+
+			if(!$company):
+
+				return Redirect::to($this->route);
+
+			else:
+
+				return View::make('backend.companies.update', array('company' = $company));
+
+			endif;
+
+		endif;
+
 	}
 
+	public function postUpdate( $id = '' ){
 
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
-		//
+		if( $id == '' ):
+
+			return Redirect::to($this->route);
+		
+		else:
+
+			$company = Companies::find($id);
+
+			if(!$company):
+
+				return Redirect::to($this->route);
+
+			else:
+
+				$company->title = Input::get('title');
+				$company->content = Input::get('content');
+				$company->type = 'company';
+				$company->status = 'draft';
+				$company->save();
+				return Redirect::to($this->route);
+
+			endif;
+
+		endif;
+
 	}
 
+	public function getPublish( $id = '' ){
 
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id)
-	{
-		//
+		if( $id == '' ):
+
+			return Redirect::to($this->route)->with('msg_error','Can\'t read publish without an identification key of companies');
+		
+		else:
+
+			$company = Companies::publish($id);
+
+			if(!$company):
+
+				return Redirect::to($this->route)->with('msg_error','Can\'t publish the company');
+
+			else:
+
+				return Redirect::to($this->route)->with('msg_success', 'Category was published successfully');
+
+			endif;
+
+		endif;
+
 	}
 
+	public function getDraft( $id = '' ){
 
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		//
+		if( $id == '' ):
+
+			return Redirect::to($this->route)->with('msg_error','Can\'t read draft without an identification key of companies');
+		
+		else:
+
+			$company = Companies::draft($id);
+
+			if(!$company):
+
+				return Redirect::to($this->route)->with('msg_error','Can\'t draft the company');
+
+			else:
+
+				return Redirect::to($this->route)->with('msg_success', 'Category was drafted successfully');
+
+			endif;
+
+		endif;
+
 	}
+
+	public function getTrash( $id = '' ){
+
+		if( $id == '' ):
+
+			return Redirect::to($this->route)->with('msg_error','Can\'t read trash without an identification key of companies');
+		
+		else:
+
+			$company = Companies::trash($id);
+
+			if(!$company):
+
+				return Redirect::to($this->route)->with('msg_error','Can\'t trash the company');
+
+			else:
+
+				return Redirect::to($this->route)->with('msg_success', 'Category was trashed successfully');
+
+			endif;
+
+		endif;
+
+	}
+
+	public function getUntrash( $id = '' ){
+
+		if( $id == '' ):
+
+			return Redirect::to($this->route)->with('msg_error','Can\'t read untrash without an identification key of companies');
+		
+		else:
+
+			$company = Companies::draft($id);
+
+			if(!$company):
+
+				return Redirect::to($this->route)->with('msg_error','Can\'t untrash the company');
+
+			else:
+
+				return Redirect::to($this->route)->with('msg_success', 'Category was untrashed successfully');
+
+			endif;
+
+		endif;
+
+	}
+
+	public function getDelete( $id = '' ){
+
+		if( $id == '' ):
+
+			return View::make('backend.companies.trash');
+
+		else:
+
+			$company = Companies::destroy($id);
+
+			if(!$company):
+
+				return Redirect::to($this->route)->with('msg_error','Can\'t delete the company');
+
+			else:
+
+				return Redirect::to($this->route)->with('msg_success', 'Category was deleted successfully');
+
+			endif;
+
+
+	}
+
 
 
 }
