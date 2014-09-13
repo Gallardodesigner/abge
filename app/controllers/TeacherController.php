e51d16ef52ecc63b5317823db14925c59644e166
AlexanderZon <alexisanderson@ovi.com>
2014-09-13 10:48:05 -0430

    TeacherController Adapter

--- app/controllers/TeacherController.php
+++ app/controllers/TeacherController.php
@@ -5,7 +5,7 @@ class TeacherController extends \BaseController {
 	
 	protected $route = '/dashboard/teachers';
 
-	public function getIndex(){
+		public function getIndex(){
 
 		$teachers = Teachers::getUntrash();
 
@@ -29,15 +29,48 @@ class TeacherController extends \BaseController {
 
 	public function postCreate(){
 
-		$teacher = new Teachers();
-		$teacher->title = Input::get('title');
-		$teacher->content = Input::get('content');
-		$teacher->type = 'teacher';
-		$teacher->status = 'draft';
-		$teacher->save();
+		$image = Input::file('url');
 
-		return View::make('backend.teachers.index', array('msg_success' => 'The teacher was successfully created!'));
+		$validator = Validator::make(
+			array(
+				'image' => $image
+				), 
+			array(
+				'image' => 'required|mimes:png,jpeg,gif'
+				),
+			array(
+				'mimes' => 'Tipo de imagen inválido, solo se admite los formatos PNG, JPEG, y GIF'
+				)
+			);
 
+		if($validator->fails()):
+
+			return Redirect::to($this->route.'/create')->with('msg_succes', Lang::get('messages.teachers_create_img_err'));
+
+		else:
+
+			$filename = $this->uploadImage($image);
+
+			$teacher = new Teachers();
+			$teacher->title = Input::get('title');
+			$teacher->content = Input::get('content');
+			$teacher->address = Input::get('address');
+			$teacher->contact = Input::get('contact');
+			$teacher->type = 'teacher';
+			$teacher->status = 'draft';
+			$teacher->url = $filename;
+
+			if($teacher->save()):
+
+				return Redirect::to($this->route)->with('msg_success', Lang::get('messages.teachers_create', array( 'title' => $teacher->title )));
+
+			else:
+
+				return Redirect::to($this->route)->with('msg_error', Lang::get('messages.teachers_create_err', array( 'title' => $teacher->title )));
+
+			endif;
+
+		endif;
 	}
 
 	public function getUpdate( $id = '' ){
@@ -52,11 +85,11 @@ class TeacherController extends \BaseController {
 
 			if(!$teacher):
 
-				return Redirect::to($this->route);
+				return Redirect::to($this->route)->with('msg_error', Lang::get('messages.teachers_display_err'));
 
 			else:
 
-				return View::make('backend.teachers.update', array('teacher' = $teacher));
+				return View::make('backend.teachers.update', array('teacher' => $teacher));
 
 			endif;
 
@@ -82,10 +115,50 @@ class TeacherController extends \BaseController {
 
 				$teacher->title = Input::get('title');
 				$teacher->content = Input::get('content');
+				$teacher->address = Input::get('address');
+				$teacher->contact = Input::get('contact');
 				$teacher->type = 'teacher';
 				$teacher->status = 'draft';
-				$teacher->save();
-				return Redirect::to($this->route);
+
+				$image = Input::file('url');
+
+				if($image != null):
+
+					$validator = Validator::make(
+						array(
+							'image' => $image
+							), 
+						array(
+							'image' => 'required|mimes:png,jpeg,gif'
+							),
+						array(
+							'mimes' => 'Tipo de imagen inválido, solo se admite los formatos PNG, JPEG, y GIF'
+							)
+						);
+
+					if($validator->fails()):
+
+						return Redirect::to($this->route.'/update/'.$id)->with('msg_succes', Lang::get('messages.teachers_update_err', array( 'title' => $teacher->title )));
+
+					else:
+
+						$filename = $this->uploadImage($image);
+
+						$teacher->url = $filename;
+					
+					endif;
+
+				endif;
+
+				if($teacher->save()):
+
+					return Redirect::to($this->route)->with('msg_succes', Lang::get('messages.teachers_update', array( 'title' => $teacher->title )));
+
+				else:
+
+					return Redirect::to($this->route)->with('msg_error', Lang::get('messages.teachers_update_err', array( 'title' => $teacher->title )));
+
+				endif;
 
 			endif;
 
@@ -97,19 +170,21 @@ class TeacherController extends \BaseController {
 
 		if( $id == '' ):
 
-			return Redirect::to($this->route)->with('msg_error','Can\'t read publish without an identification key of teachers');
+			return Redirect::to($this->route)->with('msg_error', Lang::get('messages.teachers_display_err'));
 		
 		else:
 
-			$teacher = Teachers::publish($id);
+			$teacher = Teachers::find($id);
 
-			if(!$teacher):
+			$publish = Teachers::publish($id);
 
-				return Redirect::to($this->route)->with('msg_error','Can\'t publish the teacher');
+			if(!$publish):
+
+				return Redirect::to($this->route)->with('msg_error', Lang::get('messages.teachers_publish_err', array( 'title' => $teacher->title )));
 
 			else:
 
-				return Redirect::to($this->route)->with('msg_success', 'Category was published successfully');
+				return Redirect::to($this->route)->with('msg_success', Lang::get('messages.teachers_publish', array( 'title' => $teacher->title )));
 
 			endif;
 
@@ -121,19 +196,21 @@ class TeacherController extends \BaseController {
 
 		if( $id == '' ):
 
-			return Redirect::to($this->route)->with('msg_error','Can\'t read draft without an identification key of teachers');
+			return Redirect::to($this->route)->with('msg_error', Lang::get('messages.teachers_display_err'));
 		
 		else:
 
-			$teacher = Teachers::draft($id);
+			$teacher = Teachers::find($id);
 
-			if(!$teacher):
+			$draft = Teachers::draft($id);
 
-				return Redirect::to($this->route)->with('msg_error','Can\'t draft the teacher');
+			if(!$draft):
+
+				return Redirect::to($this->route)->with('msg_error', Lang::get('messages.teachers_draft_err', array( 'title' => $teacher->title )));
 
 			else:
 
-				return Redirect::to($this->route)->with('msg_success', 'Category was drafted successfully');
+				return Redirect::to($this->route)->with('msg_success', Lang::get('messages.teachers_draft', array( 'title' => $teacher->title )));
 
 			endif;
 
@@ -145,19 +222,31 @@ class TeacherController extends \BaseController {
 
 		if( $id == '' ):
 
-			return Redirect::to($this->route)->with('msg_error','Can\'t read trash without an identification key of teachers');
+			$teachers = Teachers::getTrash();
+
+			$msg_success = Session::get('msg_success');
+
+			$msg_error = Session::get('msg_error');
+
+			return View::make('backend.teachers.trash', array(
+				'teachers' => $teachers,
+				'msg_success' => $msg_success,
+				'msg_error' => $msg_error
+				));
 		
 		else:
 
-			$teacher = Teachers::trash($id);
+			$teacher = Teachers::find($id);
 
-			if(!$teacher):
+			$trash = Teachers::trash($id);
 
-				return Redirect::to($this->route)->with('msg_error','Can\'t trash the teacher');
+			if(!$trash):
+
+				return Redirect::to($this->route)->with('msg_error', Lang::get('messages.teachers_trash_err', array( 'title' => $teacher->title )));
 
 			else:
 
-				return Redirect::to($this->route)->with('msg_success', 'Category was trashed successfully');
+				return Redirect::to($this->route)->with('msg_success', Lang::get('messages.teachers_trash', array( 'title' => $teacher->title )));
 
 			endif;
 
@@ -169,19 +258,21 @@ class TeacherController extends \BaseController {
 
 		if( $id == '' ):
 
-			return Redirect::to($this->route)->with('msg_error','Can\'t read untrash without an identification key of teachers');
+			return Redirect::to($this->route)->with('msg_error', Lang::get('messages.teachers_display_err'));
 		
 		else:
 
-			$teacher = Teachers::draft($id);
+			$teacher = Teachers::find($id);
 
-			if(!$teacher):
+			$draft = Teachers::draft($id);
 
-				return Redirect::to($this->route)->with('msg_error','Can\'t untrash the teacher');
+			if(!$draft):
+
+				return Redirect::to($this->route.'/trash')->with('msg_error', Lang::get('messages.teachers_untrash_err', array( 'title' => $teacher->title )));
 
 			else:
 
-				return Redirect::to($this->route)->with('msg_success', 'Category was untrashed successfully');
+				return Redirect::to($this->route.'/trash')->with('msg_success', Lang::get('messages.teachers_untrash', array( 'title' => $teacher->title )));
 
 			endif;
 
@@ -193,23 +284,49 @@ class TeacherController extends \BaseController {
 
 		if( $id == '' ):
 
-			return View::make('backend.teachers.trash');
+			return Redirect::to($this->route)->with('msg_error', Lang::get('messages.teachers_display_err'));
 
 		else:
 
-			$teacher = Teachers::destroy($id);
+			$teacher = Teachers::find($id);
 
-			if(!$teacher):
+			$delete = Teachers::destroy($id);
 
-				return Redirect::to($this->route)->with('msg_error','Can\'t delete the teacher');
+			if(!$delete):
+
+				return Redirect::to($this->route.'/trash')->with('msg_error', Lang::get('messages.teachers_delete_err', array( 'title' => $teacher->title )));
 
 			else:
 
-				return Redirect::to($this->route)->with('msg_success', 'Category was deleted successfully');
+				return Redirect::to($this->route.'/trash')->with('msg_success', Lang::get('messages.teachers_delete', array( 'title' => $teacher->title )));
 
 			endif;
 
+		endif;
 
+	}
+
+	public function uploadImage($image){
+
+		//dd(storage_path('uploads/'));
+
+		$info_image = getimagesize($image);
+		$ratio = $info_image[0] / $info_image[1];
+		$newheight=array();
+		$width=array("100","200","400",$info_image[0]);
+		#$filename = "prueba.".$image->getClientOriginalExtension();
+		$filename = str_replace('/', '!', Hash::make($image->getClientOriginalName().date('Y-m-d H:i:s'))).".".$image->getClientOriginalExtension();
+		$nombres=["thumb_".$filename,"small_".$filename,"medium_".$filename,$filename];
+
+		for ($i=0; $i <count($width) ; $i++):
+
+			$path = public_path('uploads/'.$nombres[$i]);
+			Image::make($image->getRealPath())->resize($width[$i],null,function ($constraint) {$constraint->aspectRatio();})->save($path);
+		
+		endfor;
+
+		return $filename;
+		
 	}
 
 
