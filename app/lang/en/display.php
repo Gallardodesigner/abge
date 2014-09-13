1149f673686b228cd65f0e43984f0a05e1040c7a
AlexanderZon <alexisanderson@ovi.com>
2014-09-13 03:48:29 -0430

    Display Languages Adapter

---
+++ app/lang/en/display.php
@@ -0,0 +1,24 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Pagination Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are used by the paginator library to build
+	| the simple pagination links. You are free to change them to anything
+	| you want to customize your views to better match your application.
+	|
+	*/
+
+	# Category Messages
+
+	'publish' => 'Published',
+
+	'draft' => 'Drafted',
+
+	'trash' => 'Trashed',
+
+);
