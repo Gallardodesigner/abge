d8f48d7e32c974bc95b595893536920d8b671a8e
Robert Da Corte <robertdacorte@gmail.com>
2014-09-18 18:26:12 -0430

    front views

---
+++ app/views/frontend/courses/promotioners.blade.php
@@ -0,0 +1,26 @@
+<!-- Layout principal -->
+@extends("frontend.courses.layout_destaque");
+
+<!-- Titulo de la pagina -->
+@section("title");
+	Cursos
+@stop
+
+<!-- Contenido principal -->
+@section("maincontent")
+	
+	<div class="columna_central_home">
+		<div class="course" data-id="{{$course->id}}">
+    <div class="course_title">
+      <h1>{{$course->event->title}} : {{$course->title}}</h1>
+      <h5>Data : {{$course->start}} a {{$course->end}} - Local : {{$course->address}}</h5>
+    </div>
+    <div id="content">
+      <div>
+        {{$course->promotioners}}
+      </div>
+    </div>
+    </div>
+	</div>
+
+@stop
