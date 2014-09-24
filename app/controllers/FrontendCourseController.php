d5d40a93a18c50d32ecea3afe25e87b4edcc96c5
AlexanderZon <alexisanderson@ovi.com>
2014-09-24 11:01:24 -0430

    Section Type adapter in FrontendCoursesController

--- app/controllers/FrontendCourseController.php
+++ app/controllers/FrontendCourseController.php
@@ -91,7 +91,7 @@ class FrontendCourseController extends \BaseController {
 
 		else:
 
-		return View::make('frontend.courses.index')->with( $array );
+		return View::make('frontend.courses.content')->with( $array );
 
 		endif;
 
