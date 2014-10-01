79edefc55bc4e4c01c4deca680dececa6d39ab09
Robert Da Corte <robertdacorte@gmail.com>
2014-10-01 16:05:22 -0430

    Fix Auth Views

--- app/views/auth/register.blade.php
+++ app/views/auth/register.blade.php
@@ -15,53 +15,69 @@
       <!-- <h5>Data : {{$course->start}} a {{$course->end}} - Local : {{$course->address}}</h5> -->
     </div>
     <div id="content">
-    <form method="post">
+    <form class="updateform" method="post">
     	<input type="hidden" name="usertype" value="{{$usertype->id}}"/>
     	<input type="hidden" name="course" value="{{$course->id}}"/>
-		<label>Nome: </label><input type="text" name="nome"/><br>
-		<label>Email: </label><input type="email" name="email"/><br>
-		<label>RG: </label><input type="text" name="rg"/><br>
-		<label>CPF: </label><input type="text" name="cpf" value="{{ $cpf }}"/><br>
-		<label>Endereço: </label><input type="text" name="endereco"/><br>
-		<label>N°: </label><input type="text" name="numero"/><br>
-		<label>Complemento: </label><input type="text" name="complemento"/><br>
-		<label>CEP: </label><input type="text" name="cep"/><br>
-		<label>Estado: </label>
-			<select name="estado">
-				<option value="0" selected>Seleccione um estado</option>
-				@foreach($estados as $state)
-					<option value="{{$state->id_estado}}">{{$state->name_estado}}</option>
-				@endforeach
-			</select>
-			<br>
-		<label>Cidade: </label><input type="text" name="cidade"/><br>
-		<div><h3>Informações de Empresa</h3></div>
-		<label>Empresa: </label><input type="text" name="empresa"/><br>
-		<label>CNPJ: </label><input type="text" name="cnpj_empresa"/><br>
-		<label>Endereço: </label><input type="text" name="endereco_empresa"/><br>
-		<label>N°: </label><input type="text" name="numero"/><br>
-		<label>Complemento: </label><input type="text" name="complemento_empresa"/><br>
-		<label>CEP: </label><input type="text" name="cep_empresa"/><br>
-		<label>Estado: </label>
-			<select name="estado_empresa">
-				<option value="0" selected>Seleccione um estado</option>
-				@foreach($estados as $state)
-					<option value="{{$state->id_estado}}">{{$state->name_estado}}</option>
-				@endforeach
-			</select>
-			<br>
-		<label>Cidade: </label><input type="text" name="cidade_empresa"/><br>
-		<label>Telefone: </label><input type="text" name="telefone_empresa"/><br>
-		<label>Celular: </label><input type="text" name="celular_empresa"/><br>
-		<label>Tipo de participante: </label><select name="type">
-			<option value="0" selected>Seleccione um tipo</option>
-			@foreach($course->usertypes as $user)
-				@if(!$user->associate)
-			<option value="{{ $user->id }}">{{$user->title}}</option>
-				@endif
-			@endforeach
-		</select><br>
-		<input type="submit" value="Enviar"/>
+    	<div class="control-box">
+			<label>Nome: </label>
+			<input type="text" name="nome"/>
+		</div>
+    	<div class="control-box">
+			<label>Email: </label>
+			<input type="email" name="email"/>
+		</div>
+    	<div class="control-box">
+			<label>RG: </label>
+			<input type="text" name="rg"/>
+		</div>
+    	<div class="control-box">
+			<label>CPF: </label>
+			<input type="text" name="cpf" value="{{ $cpf }}"/>
+		</div>
+    	<div class="control-box">
+			<label>Endereço: </label>
+			<input type="text" name="endereco"/>
+		</div>
+    	<div class="control-box">
+			<label>N°: </label>
+			<input type="text" name="numero"/>
+		</div>
+    	<div class="control-box">
+			<label>Complemento: </label>
+			<input type="text" name="complemento"/>
+		</div>
+    	<div class="control-box">
+			<label>CEP: </label>
+			<input type="text" name="cep"/>
+		</div>
+    	<div class="control-box">
+			<label>Estado: </label>
+				<select name="estado">
+					<option value="0" selected>Seleccione um estado</option>
+					@foreach($estados as $state)
+						<option value="{{$state->id_estado}}">{{$state->name_estado}}</option>
+					@endforeach
+				</select>
+		</div>
+    	<div class="control-box">
+			<label>Cidade: </label>
+			<input type="text" name="cidade"/>
+		</div>
+    	<div class="control-box">
+			<label>Empresa: </label>
+			<input type="text" name="empresa"/>
+		</div>
+    	<div class="control-box">
+			<label>Telefone: </label>
+			<input type="text" name="telefone_empresa"/>
+		</div>
+    	<div class="control-box">
+			<label>Celular: </label>
+			<input type="text" name="celular_empresa"/>
+		</div>
+    	<div class="control-box">
+			<input type="submit" value="Enviar"/>
+		</div>
 	</form>
     </div>
     </div>
