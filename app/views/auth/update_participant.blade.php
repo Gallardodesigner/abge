eef26c8081fd8f097f1c15fcab31a606d1010b39
Robert Da Corte <robertdacorte@gmail.com>
2014-10-01 15:52:05 -0430

    Fix Auth Views

--- app/views/auth/update_participant.blade.php
+++ app/views/auth/update_participant.blade.php
@@ -26,16 +26,38 @@
     	<input type="hidden" name="inscription" value="{{$inscription->id}}"/>
 		<div class="control-box">
 			<label>Nome: </label>
-			<input type="text" name="nome" value="{{ $participant->nome }}"/><br>
+			<input type="text" name="nome" value="{{ $participant->nome }}"/>
 		</div>
-		<label>Email: </label><input type="email" name="email" value="{{ $participant->email }}"/><br>
-		<label>RG: </label><input type="text" name="rg" value="{{ $participant->rg }}"/><br>
-		<label>CPF: </label><input type="text" name="cpf" value="{{ $participant->cpf }}"/><br>
-		<label>Endereço: </label><input type="text" name="endereco" value="{{ $participant->endereco }}"/><br>
-		<label>N°: </label><input type="text" name="numero" value="{{ $participant->numero }}"/><br>
-		<label>Complemento: </label><input type="text" name="complemento" value="{{ $participant->complemento }}"/><br>
-		<label>CEP: </label><input type="text" name="cep" value="{{ $participant->cep }}"/><br>
-		<label>Estado: </label>
+		<div class="control-box">
+			<label>Email: </label>
+			<input type="email" name="email" value="{{ $participant->email }}"/>
+		</div>
+		<div class="control-box">
+			<label>RG: </label>
+			<input type="text" name="rg" value="{{ $participant->rg }}"/>
+		</div>
+		<div class="control-box">
+			<label>CPF: </label>
+			<input type="text" name="cpf" value="{{ $participant->cpf }}"/>
+		</div>
+		<div class="control-box">
+			<label>Endereço: </label>
+			<input type="text" name="endereco" value="{{ $participant->endereco }}"/>
+		</div>
+		<div class="control-box">
+			<label>N°: </label>
+			<input type="text" name="numero" value="{{ $participant->numero }}"/>
+		</div>
+		<div class="control-box">
+			<label>Complemento: </label>
+			<input type="text" name="complemento" value="{{ $participant->complemento }}"/>
+		</div>
+		<div class="control-box">
+			<label>CEP: </label>
+			<input type="text" name="cep" value="{{ $participant->cep }}"/>
+		</div>
+		<div class="control-box">
+			<label>Estado: </label>
 			<select name="estado">
 				<option value="0" selected>Seleccione um estado</option>
 				@foreach($estados as $state)
@@ -46,11 +68,23 @@
 					@endif
 				@endforeach
 			</select>
-			<br>
-		<label>Cidade: </label><input type="text" name="cidade" value="{{ $participant->cidade }}"/><br>
-		<div><h3>Informações de Empresa</h3></div>
-		<label>Empresa: </label><input type="text" name="empresa" value="{{ $participant->empresa }}"/><br>
-		<label>CNPJ: </label><input type="text" name="cnpj_empresa" value="{{ $participant->cnpj_empresa }}"/><br>
+		</div>
+		<div class="control-box">
+			<label>Cidade: </label>
+			<input type="text" name="cidade" value="{{ $participant->cidade }}"/>
+		</div>
+		<div class="control-box">
+			<label>Telefone: </label>
+			<input type="text" name="telefone_empresa" value="{{ $participant->telefone_empresa }}"/>
+		</div>
+		<div class="control-box">
+			<label>Celular: </label>
+			<input type="text" name="celular_empresa" value="{{ $participant->celular_empresa }}"/>
+		</div>
+		<div class="control-box">
+			<label>Empresa: </label><input type="text" name="empresa" value="{{ $participant->empresa }}"/>
+		</div>
+		<!-- <label>CNPJ: </label><input type="text" name="cnpj_empresa" value="{{ $participant->cnpj_empresa }}"/><br>
 		<label>Endereço: </label><input type="text" name="endereco_empresa" value="{{ $participant->endereco_empresa }}"/><br>
 		<label>N°: </label><input type="text" name="numero_empresa" value="{{ $participant->numero_empresa }}"/><br>
 		<label>Complemento: </label><input type="text" name="complemento_empresa" value="{{ $participant->complemento_empresa }}"/><br>
@@ -67,18 +101,18 @@
 				@endforeach
 			</select>
 			<br>
-		<label>Cidade: </label><input type="text" name="cidade_empresa" value="{{ $participant->cidade_empresa }}"/><br>
-		<label>Telefone: </label><input type="text" name="telefone_empresa" value="{{ $participant->telefone_empresa }}"/><br>
-		<label>Celular: </label><input type="text" name="celular_empresa" value="{{ $participant->celular_empresa }}"/><br>
-		<label>Tipo de participante: </label><select name="type">
+		<label>Cidade: </label><input type="text" name="cidade_empresa" value="{{ $participant->cidade_empresa }}"/><br> -->
+		<!-- <label>Tipo de participante: </label><select name="type">
 			<option value="0" selected>Seleccione um tipo</option>
 			@foreach($course->usertypes as $user)
 				@if(!$user->associate)
 					<option value="{{ $user->id }}">{{$user->title}}</option>
 				@endif
 			@endforeach
-		</select><br>
-		<input type="submit" value="Enviar"/>
+		</select><br> -->
+		<div class="control-box">
+			<input type="submit" value="Enviar"/>
+		</div>
 	</form>
     </div>
     </div>
