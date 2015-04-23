408de6a9684b98864ea79335665a441aebd5af92
AlexanderZon <theguitarplayer.am@gmail.com>
2015-04-23 08:42:54 -0430

    Frontend ajax

---
+++ app/controllers/FrontendAjaxController.php
@@ -0,0 +1,145 @@
+<?php
+
+class FrontendAjaxController extends \BaseController {
+
+	public function getAdicionarescolaridade($id_asociado){
+
+		$array = array(
+			'id_asociado' => $id_asociado,
+			'formacoes' => ORGTrainings::all(),
+			);
+
+		return View::make('ajax.frontend.adicionarescolaridade')->with($array);
+
+	}
+
+	public function postAdicionarescolaridade($id_asociado){
+
+		$academic = new ORGAcademics();
+		$academic->tipo_graduacion = Input::get('tipo_graduacion');
+		$academic->institucion = Input::get('institucion');
+		$academic->facultad = Input::get('facultad');
+		$academic->curso_realizado = Input::get('curso_realizado');
+		$academic->ano_inicio = Input::get('ano_inicio');
+		$academic->ano_finalizacion = Input::get('ano_finalizacion');
+		$academic->id_asociado = $id_asociado;
+		
+		if($academic->save()):
+			$curso_realizado = ORGTrainings::find($academic->curso_realizado);
+			$tipo_graduacion = '';
+			switch($academic->tipo_graduacion){
+				case 0:
+					$tipo_graduacion = 'DOUTORADO';
+					break;
+				case 1:
+					$tipo_graduacion = 'ESPECIALIZAÇÃO';
+					break;
+				case 2:
+					$tipo_graduacion = 'GRADUAÇÃO';
+					break;
+				case 3: 
+					$tipo_graduacion = 'MESTRADO';
+					break;
+				case 4:
+					$tipo_graduacion = 'PÓS-GRADUAÇÃO';
+					break;
+			}
+			$json = array(
+				'id_datos_acad' => $academic->id_datos_acad,
+				'tipo_graduacion' => $tipo_graduacion,
+				'institucion' => $academic->institucion,
+				'facultad' => $academic->facultad,
+				'curso_realizado' => $curso_realizado->nome,
+				'ano_inicio' => $academic->ano_inicio,
+				'ano_finalizacion' => $academic->ano_finalizacion
+				);
+			return Response::json($json);
+		else:
+			return false;
+		endif;
+
+	}
+
+	public function getAtualizarescolaridade($id_escolaridade){
+
+		$array = array(
+			'academic' => ORGAcademics::find($id_escolaridade),
+			'formacoes' => ORGTrainings::all(),
+			);
+
+		return View::make('ajax.frontend.atualizarescolaridade')->with($array);
+
+	}
+
+	public function postAtualizarescolaridade($id_escolaridade){
+
+		$academic = ORGAcademics::find($id_escolaridade);
+		$academic->tipo_graduacion = Input::get('tipo_graduacion');
+		$academic->institucion = Input::get('institucion');
+		$academic->facultad = Input::get('facultad');
+		$academic->curso_realizado = Input::get('curso_realizado');
+		$academic->ano_inicio = Input::get('ano_inicio');
+		$academic->ano_finalizacion = Input::get('ano_finalizacion');
+		
+		if($academic->save()):
+			$curso_realizado = ORGTrainings::find($academic->curso_realizado);
+			$tipo_graduacion = '';
+			switch($academic->tipo_graduacion){
+				case 0:
+					$tipo_graduacion = 'DOUTORADO';
+					break;
+				case 1:
+					$tipo_graduacion = 'ESPECIALIZAÇÃO';
+					break;
+				case 2:
+					$tipo_graduacion = 'GRADUAÇÃO';
+					break;
+				case 3: 
+					$tipo_graduacion = 'MESTRADO';
+					break;
+				case 4:
+					$tipo_graduacion = 'PÓS-GRADUAÇÃO';
+					break;
+			}
+			$json = array(
+				'id_datos_acad' => $academic->id_datos_acad,
+				'tipo_graduacion' => $tipo_graduacion,
+				'institucion' => $academic->institucion,
+				'facultad' => $academic->facultad,
+				'curso_realizado' => $curso_realizado->nome,
+				'ano_inicio' => $academic->ano_inicio,
+				'ano_finalizacion' => $academic->ano_finalizacion
+				);
+			return Response::json($json);
+		else:
+			return false;
+		endif;
+
+	}
+
+	public function getDeletarescolaridade($id_escolaridade){
+
+		$array = array(
+			'academic' => ORGAcademics::find($id_escolaridade)
+			);
+
+		return View::make('ajax.frontend.deletarescolaridade')->with($array);
+
+	}
+
+	public function postDeletarescolaridade($id_escolaridade){
+
+		$academic = ORGAcademics::find($id_escolaridade);
+		
+		if($academic->delete()):
+			$array = array(
+				'id_datos_acad' => $academic->id_datos_acad
+				);
+			return Response::json($array);
+		else:
+			return Response::json(false);
+		endif;
+
+	}
+
+}
---
+++ app/views/ajax/frontend/adicionarescolaridade.blade.php
@@ -0,0 +1,140 @@
+<form id="adicionarescolaridade">
+    <div id="wiz1step5" class="formwiz">
+        <h4 class="widgettitle">Adicionar Escolaridade</h4>
+        <p>
+            <label class="control-label">Tipo de graduação</label>
+            <span>
+                <select class="form-control" name="tipo_graduacion" id="asociados_datos_academicos_tipo_graduacion" required>
+                    <option value="0">DOUTORADO</option>
+                    <option value="1">ESPECIALIZAÇÃO</option>
+                    <option value="2">GRADUAÇÃO</option>
+                    <option value="3">MESTRADO</option>
+                    <option value="4">PÓS-GRADUAÇÃO</option>
+                </select>
+            </span>
+        </p>
+        <p>
+            <label class="control-label">Instituição:</label>
+            <span class="field"><input type="text" name="institucion" id="institucion" class="form-control" required/></span>
+        </p>
+        <p>
+            <label class="control-label">Faculdade:</label>
+            <span class="field"><input type="text" name="facultad" id="facultad" class="form-control" required/></span>
+        </p>
+        <p>
+            <label class="control-label">Formação:</label>
+            <span class="field">
+                @if (isset($formacoes))
+                    <select class="form-control" name="curso_realizado">
+                        @foreach ($formacoes as $formacao)
+                            <option value="{{$formacao->id}}">{{$formacao->nome}}</option>
+                        @endforeach
+                    </select>
+                @endif
+            </span>
+        </p>
+        <p>
+            <label class="control-label">Ano de início</label>
+            <span class="field"><input type="text" name="ano_inicio" id="ano_inicio" class="form-control" required/></span>
+        </p>
+        <p>
+            <label class="control-label">Ano de finalização</label>
+            <span class="field"><input type="text" name="ano_finalizacion" id="ano_finalizacion" class="form-control" required/></span>
+        </p>  
+        <p>
+            
+            <span class="right"><input type="submit" class="btn btn-primary" value="Adicionar"/></span>
+        </p>                                                  
+    </div>
+</form>
+<div id="loader_container" style="display:none; ">
+	<img src="/assets/fancybox/fancybox_loading.gif" id="loading_image"/>
+</div>
+<style type="text/css">
+	#loader_container{
+		width: 100%;
+		height: 300px;
+		padding-top: 150px;
+		display:none;
+		background-color: rgba(255,255,255,0.2);
+		background-position: cover;
+		text-align:center;
+		vertical-align: middle;
+	}
+</style>
+
+<script type="text/javascript">
+    jQuery('#adicionarescolaridade').on('submit', function(e){
+    	jQuery('#adicionarescolaridade').ajaxStart(function(e){
+    		jQuery('#adicionarescolaridade').css({
+    			'display':'none'
+    		});
+    		jQuery('#loader_container').css({
+    			'display':'block'
+    		});
+    	});
+    	jQuery('#adicionarescolaridade').ajaxStop(function(e){
+    		jQuery('#loader_container').css({
+    			'display':'none'
+    		});
+    	});
+    	e.preventDefault();
+    	jQuery.ajax({
+    		url: '/ajax/adicionarescolaridade/{{ $id_asociado }}',
+    		type: 'post',
+    		data: jQuery(this).serialize(),
+    		success: function(data){
+    			jQuery('#adicionarbutton').before('<div id="esolaridade_'+data.id_datos_acad+'" style="margin-left: 5em;display:inline-block">'+
+                                            '<p>'+
+                                                '<b>Tipo de graduação:</b> '+
+                                                '<span id="tipo_graduacion_'+data.id_datos_acad+'">'+
+                                                	data.tipo_graduacion+
+                                                '</span>'+
+                                            '</p>'+
+                                            '<p>'+
+                                                '<b>Instituição:</b> '+
+                                                '<span id="institucion_'+data.id_datos_acad+'">'+
+                                                    data.institucion+
+                                                '</span>'+
+                                            '</p>'+
+                                            '<p>'+
+                                                '<b>Faculdade:</b> '+
+                                                '<span id="facultad_'+data.id_datos_acad+'">'+
+                                                    data.facultad+
+                                                '</span>'+
+                                            '</p>'+
+                                            '<p>'+
+                                                '<b>Formação:</b> '+
+                                                '<span id="curso_realizado_'+data.id_datos_acad+'">'+
+                                                   data.curso_realizado+
+                                                '</span>'+
+                                            '</p>'+
+                                            '<p>'+
+                                                '<b>Ano de início:</b> '+
+                                                '<span id="ano_inicio_'+data.id_datos_acad+'">'+
+                                                    data.ano_inicio+
+                                                '</span>'+
+                                            '</p>'+
+                                            '<p>'+
+                                                '<b>Ano de finalização:</b> '+
+                                                '<span id="ano_finalizacion_'+data.id_datos_acad+'">'+
+                                                    data.ano_finalizacion+
+                                                '</span>'+
+                                            '</p>'+
+                                            '<p>'+
+                                                '<span>'+
+                                                    '<a href="/ajax/atualizarescolaridade/'+data.id_datos_acad+'" class="fancybox fancybox.ajax btn" id="adicionar_escolaridade" >Atualizar</a>'+
+                                                    '<a href="/ajax/deletarescolaridade/'+data.id_datos_acad+'" class="fancybox fancybox.ajax btn" id="adicionar_escolaridade" >Deletar</a>'+
+                                                '</span>'+
+                                            '</p>'+
+                                        '</div>');
+    			console.log(data);
+    			jQuery('.fancybox-close').click();
+    		},
+    		error: function(e){	
+    			console.log(e);
+    		}
+    	})
+    	console.log('Fancy ready');
+    });
+</script>
